fb60f534d39ff5fa9a35b3f57728b028ad951429
Chris Ford <chrisf@primate.com>
2016-09-05 17:14:59 +0100

    alphabet cipher first pass

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,54 @@
 (ns alphabet-cipher.coder)
 
+(def  baseseq (seq "abcdefghijklmnopqrstuvwxyz")) 
+
+(defn expand [ keyword ] (cycle keyword))
+
+(defn character_index [ inchar inseq ]
+  (let [ searchchar (Character/toLowerCase inchar)]
+    (first  (keep-indexed (fn [index x] (when (= x searchchar) index)) inseq))))
+
+(defn row_seq [inchar]
+    (let [ index   (character_index inchar baseseq)
+           back     (take index baseseq) 
+           front   (drop index baseseq) ] 
+      (concat front back)))
+
+
+
+(defn encode_kmpair [ [kchar mchar] ] 
+  (nth (row_seq kchar) (character_index mchar baseseq)  ) )
+
+(defn decode_kmpair [ [kchar mchar] ] 
+  (nth baseseq (character_index mchar (row_seq kchar))  ) )
+
+(defn decipher_cmpair [ [cchar mchar] ] 
+  (nth baseseq (character_index cchar (row_seq mchar)) ) )
+
+
+(defn make_km_pairs [ keyword message ]
+  (partition 2 (interleave (expand keyword) message )))
+
 (defn encode [keyword message]
-  "encodeme")
+  (let [ kmpairs (make_km_pairs keyword message) ]
+    (apply str (map encode_kmpair kmpairs ))))
 
 (defn decode [keyword message]
-  "decodeme")
+  (let [ kmpairs (make_km_pairs keyword message) ]
+    (apply str (map decode_kmpair kmpairs ))))
+
+(defn decipher_expanded [cipher message]
+  (let [ cmpairs (make_km_pairs cipher message)]
+    (map decipher_cmpair cmpairs )))
+
+(defn minimise_keyword [expanded]
+    (loop [ n 1 ]
+      (let [ keyword (take n expanded) 
+             totest  (make_km_pairs keyword expanded)]
+      (if (every? (fn [[a b]] (= a b)) totest)
+        (apply str keyword)
+        (recur (inc n))))))  
 
 (defn decipher [cipher message]
-  "decypherme")
+  (minimise_keyword ( decipher_expanded cipher message ) ))
 
--- alphabet-cipher/test/alphabet_cipher/coder_test.clj
+++ alphabet-cipher/test/alphabet_cipher/coder_test.clj
@@ -2,6 +2,37 @@
   (:require [clojure.test :refer :all]
             [alphabet-cipher.coder :refer :all]))
 
+(deftest test-expand
+  (testing "that the keyword can be expanded"
+    (is (= (seq  "vigilancevigilance")
+           (take 18 (expand "vigilance"))))))
+
+(deftest test-character_index 
+  (testing "that characters may be converted to an index"
+    (is ( = 0
+            (character_index \a baseseq)))
+    (is ( = 1
+            (character_index \b baseseq)))
+    (is ( = 25
+            (character_index \z baseseq)))
+    (is ( = 2
+            (character_index \C baseseq)))
+    (is ( = 24
+            (character_index \Y baseseq)))
+    ))
+
+(deftest test-row_seq
+  (testing "that we can generate the rotated input sequence")
+  (is ( = (seq  "abcdefghijklmnopqrstuvwxyz")
+          (row_seq \a)))
+  (is ( = (seq  "efghijklmnopqrstuvwxyzabcd")
+          (row_seq \e)))
+  (is ( = (seq  "zabcdefghijklmnopqrstuvwxy")
+          (row_seq \z)))
+  (is ( = (seq  "abcdefghijklmnopqrstuvwxyz")
+          (row_seq \A)))
+  )
+
 (deftest test-encode
   (testing "can encode given a secret keyword"
     (is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"
@@ -22,3 +53,4 @@
            (decipher "opkyfipmfmwcvqoklyhxywgeecpvhelzg" "thequickbrownfoxjumpsoveralazydog")))
     (is (= "scones"
            (decipher "hcqxqqtqljmlzhwiivgbsapaiwcenmyu" "packmyboxwithfivedozenliquorjugs")))))
+
