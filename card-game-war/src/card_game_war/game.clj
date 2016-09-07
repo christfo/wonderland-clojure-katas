97a46a5b749cc98552adf5e1c69ea8b8ff535989
Chris Ford <chrisf@primate.com>
2016-09-07 16:54:36 +0100

    Improvement for card-war? now deck is a map of 'rank/suit' sets to value. Makes for easy comparison in a hand. The dealing was broken for remainder cards, now fixed and covered in test. Still not too happy about play-game coercing rest to a vector to make the conj work, but at least it is correct

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -3,26 +3,23 @@
 ;; feel free to use these cards or use your own data structure
 (def suits [:spade :club :diamond :heart])
 (def ranks [2 3 4 5 6 7 8 9 10 :jack :queen :king :ace])
-(def cards
-  (for [suit suits
-        rank ranks]
-    [suit rank]))
+
+; map of card (as a set of suit and rank) to value
+(def deck
+  (into {} (map-indexed  (fn [index card] [card index])
+        (for [rank ranks suit suits ] #{ suit rank } ))))
 
 (defn deal 
-  ( [players] (partition (quot (count cards) players) (shuffle cards)))
-  ( []        (deal 2)))
-
-(defn rank2value [rank]
-  (first (keep-indexed (fn [index item] (when (= rank item) index)) ranks)))
-
-(defn suit2value [suit]
-  (first (keep-indexed (fn [index item] (when (= suit item) index)) suits)))
-
-(defn play2value [[ suit rank]]
-  (+ (* (count suits) (rank2value rank)) (suit2value suit)))
+  ( []        (deal 2))
+  ( [players] (loop [ todeal  (shuffle (keys deck)) 
+                      hands   [] 
+                      round   players] 
+                (if (> round 0) 
+                  (recur (rest todeal) (conj hands (take-nth players todeal) ) (dec round))
+                  hands))))
 
 (defn play-round [ play1 play2 ]
-  (if (> (play2value play1) (play2value play2)) 1 2))
+  (if (> (deck play1) (deck play2)) 1 2))
 
 (defn play-game 
   ( [ [hand1 hand2] ] (play-game hand1 hand2))
@@ -32,7 +29,7 @@
       (or 
         (cond (not first1) 2 (not first2) 1) ; no cards in hand
         (case (play-round first1 first2)
-          1 (recur (conj (vec rest1) first1 first2)    rest2) 
-          2 (recur  rest1                             (conj (vec  rest2) first2 first1)) )))))
+          1 (recur (conj (vec rest1) first1 first2)  rest2) 
+          2 (recur  rest1                           (conj (vec  rest2) first2 first1)) )))))
 
 
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -5,62 +5,66 @@
 
 
 ;; fill in  tests for your game
-(deftest test-rank2value
-  (testing "rank value"  (is (= 0   (rank2value 2))))
-  (testing "rank value"  (is (= 6   (rank2value 8))))
-  (testing "jack value"  (is (= 9   (rank2value :jack))))
-  (testing "queen value" (is (= 10  (rank2value :queen))))
-  (testing "king value"  (is (= 11  (rank2value :king))))
-  (testing "ace value"   (is (= 12  (rank2value :ace))))
-  (testing "bad value"   (is (= nil (rank2value :spade))))
-  (testing "bad value"   (is (= nil (rank2value 1))))
-  (testing "bad value"   (is (= nil (rank2value 11))))
-  )
 
-(deftest test-suit2value
-  (testing "spade value"   (is (= 0   (suit2value :spade))))
-  (testing "club value"    (is (= 1   (suit2value :club))))
-  (testing "diamond value" (is (= 2   (suit2value :diamond))))
-  (testing "heart value"   (is (= 3   (suit2value :heart))))
-  (testing "bad value"     (is (= nil (suit2value :ace))))
-  (testing "bad value"     (is (= nil (suit2value 5))))
-  )
-
-(deftest test-play-round
+(deftest test-deal
   (testing "dealing the cards"
     (is (count (deal 1)) 1)
     (is (count (deal 2)) 2)
     (is (count (deal 3)) 3)
-    (is (count (deal)) 2))
+    (is (count (deal))   2)))
     
+(deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 1 (play-round [:diamond 8] [:diamond 3]) ))
-    (is (= 2 (play-round [:diamond 3] [:diamond 8]) )))
+    (is (= 1 (play-round #{:diamond 8} #{:diamond 3}) ))
+    (is (= 2 (play-round #{:diamond 3} #{:diamond 8}) )))
   (testing "queens are higher rank than jacks"
-    (is (= 1 (play-round [:diamond :queen] [:diamond :jack]) )))
+    (is (= 1 (play-round #{:diamond :queen} #{:diamond :jack}) )))
   (testing "kings are higher rank than queens"
-    (is (= 1 (play-round [:spade :king] [:spade :queen]) )))
+    (is (= 1 (play-round #{:spade :king} #{:spade :queen}) )))
   (testing "aces are higher rank than kings"
-    (is (= 1 (play-round [:club :ace] [:heart :king]) )))
+    (is (= 1 (play-round #{:club :ace} #{:heart :king}) )))
   (testing "if the ranks are equal, clubs beat spades"
-    (is (= 1 (play-round [:club 8] [:spade 8]) )))
+    (is (= 1 (play-round #{:club 8} #{:spade 8}) )))
   (testing "if the ranks are equal, diamonds beat clubs"
-    (is (= 1 (play-round [:diamond :ace] [:club :ace]) )))
+    (is (= 1 (play-round #{:diamond :ace} #{:club :ace}) )))
   (testing "if the ranks are equal, hearts beat diamonds"
-    (is (= 1 (play-round [:heart :jack] [:diamond :jack]) ))))
+    (is (= 1 (play-round #{:heart :jack} #{:diamond :jack}) ))))
 
 (deftest test-play-game
   (testing "the player loses when they run out of cards"
-    (is (= 1 (play-game [ [:diamond 3] ]    [] )))
-    (is (= 2 (play-game []                  [ [:club :king] ]  )))
-    (is (= 2 (play-game [ [:diamond 3] ]    [ [:club :king] ]  )))
+    (is (= 1 (play-game [ #{:diamond 3} ]  [                ]  )))
+    (is (= 2 (play-game [               ]  [ #{:club :king} ]  )))
+    (is (= 2 (play-game [ #{:diamond 3} ]  [ #{:club :king} ]  )))
     (is (#{1 2} (play-game (deal))))
     ))
 
 
 ; try out facts...
+
+(fact "dealing the cards splits them between players"
+      (deal 1)                  => #(= 1 (count %))
+      (deal 2)                  => #(= 2 (count %))
+      (deal 3)                  => #(= 3 (count %))
+      (deal)                    => #(= 2 (count %))
+      (deal)                    => #(= [26 26]       (map count %))
+      (deal 3)                  => #(= [18 17 17]    (map count %))
+      (deal 4)                  => #(= [13 13 13 13] (map count %))
+      (set (flatten  (deal 3))) => #(= 52 (count %))
+      )
+    
+(fact "When playing a round, the highest value card wins"
+      (play-round #{:diamond 3} #{:diamond 8} )         => 2
+      (play-round #{:diamond 5} #{:diamond 2} )         => 1
+      (play-round #{:heart 5}   #{:diamond 5} )         => 1
+      (play-round #{:diamond :queen} #{:diamond :jack}) => 1
+      (play-round #{:spade :king} #{:spade :queen})     => 1
+      (play-round #{:club :ace} #{:heart :king})        => 1
+      (play-round #{:club 8} #{:spade 8})               => 1
+      (play-round #{:diamond :ace} #{:club :ace})       => 1
+      (play-round #{:heart :jack} #{:diamond :jack})    => 1)
+
 (fact "the player loses when they run out of cards"
-      (play-game [ [:diamond 3] ]   [])                  => 1
-      (play-game []                 [ [:club :king] ]  ) => 2
-      (play-game [ [:diamond 3] ]   [ [:club :king] ]  ) => 2
+      (play-game [ #{:diamond 3} ]  [                ] ) => 1
+      (play-game []                 [ #{:club :king} ] ) => 2
+      (play-game [ #{:diamond 3} ]  [ #{:club :king} ] ) => 2
       (play-game (deal))                                 => #(some #{%} [1 2]))
