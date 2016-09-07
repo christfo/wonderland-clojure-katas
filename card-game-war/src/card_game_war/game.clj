a6dca280c0a186e3b3e174ec3d6037078f985186
Chris Ford <chrisf@primate.com>
2016-09-07 01:32:55 +0100

    working card war solution

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -18,7 +18,21 @@
 (defn suit2value [suit]
   (first (keep-indexed (fn [index item] (when (= suit item) index)) suits)))
 
-(defn play-round [ [suite1 rank1] [suite2 rank2] ]
-  (when ()))
+(defn play2value [[ suit rank]]
+  (+ (* (count suits) (rank2value rank)) (suit2value suit)))
 
-(defn play-game [player1-cards player2-cards])
+(defn play-round [ play1 play2 ]
+  (if (> (play2value play1) (play2value play2)) 1 2))
+
+(defn play-game 
+  ( [ [hand1 hand2] ] (play-game hand1 hand2))
+  ( [player1-cards player2-cards]
+    (loop [ [first1 & rest1] player1-cards 
+            [first2 & rest2] player2-cards ] 
+      (or 
+        (cond (not first1) 2 (not first2) 1) ; no cards in hand
+        (case (play-round first1 first2)
+          1 (recur (conj (vec rest1) first1 first2)    rest2) 
+          2 (recur  rest1                             (conj (vec  rest2) first2 first1)) )))))
+
+
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -1,4 +1,5 @@
-(ns card-game-war.game-test
+(ns card-game-war.game-test 
+  (:use midje.sweet)
   (:require [clojure.test :refer :all]
             [card-game-war.game :refer :all]))
 
@@ -33,14 +34,33 @@
     (is (count (deal)) 2))
     
   (testing "the highest rank wins the cards in the round"
-    (is (= 1 (play-round [:diamond 8] [:diamond 3]) )))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= 1 (play-round [:diamond 8] [:diamond 3]) ))
+    (is (= 2 (play-round [:diamond 3] [:diamond 8]) )))
+  (testing "queens are higher rank than jacks"
+    (is (= 1 (play-round [:diamond :queen] [:diamond :jack]) )))
+  (testing "kings are higher rank than queens"
+    (is (= 1 (play-round [:spade :king] [:spade :queen]) )))
+  (testing "aces are higher rank than kings"
+    (is (= 1 (play-round [:club :ace] [:heart :king]) )))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= 1 (play-round [:club 8] [:spade 8]) )))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= 1 (play-round [:diamond :ace] [:club :ace]) )))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= 1 (play-round [:heart :jack] [:diamond :jack]) ))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "the player loses when they run out of cards"
+    (is (= 1 (play-game [ [:diamond 3] ]    [] )))
+    (is (= 2 (play-game []                  [ [:club :king] ]  )))
+    (is (= 2 (play-game [ [:diamond 3] ]    [ [:club :king] ]  )))
+    (is (#{1 2} (play-game (deal))))
+    ))
 
+
+; try out facts...
+(fact "the player loses when they run out of cards"
+      (play-game [ [:diamond 3] ]   [])                  => 1
+      (play-game []                 [ [:club :king] ]  ) => 2
+      (play-game [ [:diamond 3] ]   [ [:club :king] ]  ) => 2
+      (play-game (deal))                                 => #(some #{%} [1 2]))
