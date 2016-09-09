f352e5cc58ca58bdbf6c22e77abc52ec964df3f7
Chris Ford <chrisf@primate.com>
2016-09-09 16:07:35 +0100

    doublets solution

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -7,5 +7,36 @@
                (slurp)
                (read-string)))
 
+(defn relatable [word] (let [n (count word)] (filter #(= n (count %)) words)))
+
+(defn is-transition? [from to] 
+  (if (= (count from) (count to))
+    (let  [  differlist            (map not= from to)  
+            [differs & remaining]  (drop-while false? differlist) ]
+      (and differs (empty? (drop-while false? remaining))))))
+
+(defn expand-node 
+  ( [node]             ( expand-node (relatable (first node)) node) )
+  ( [dictionary node] 
+   (let [ transitions        (filter #(is-transition? % (first node)) dictionary)  
+         valid_transitions  (filter #(not-any? #{%} node)            transitions) ]
+     (map #(cons % node) valid_transitions) )))
+
+
+(defn gen-next-layer 
+  ( [nodes]            (gen-next-layer (relatable (first (first nodes))) nodes))
+  ( [candidates nodes] (mapcat (partial expand-node candidates) nodes)))
+
+
+(defn solution? [nodes destination] 
+  (some #(when (= (first %) destination) %) nodes))
+
+
 (defn doublets [word1 word2]
-  "make me work")
+  (let [ candidates   (relatable word2) ]
+    (loop [ nodes  (list [word2]) ] 
+      (if  (not-empty nodes)
+        (or  
+          (solution? nodes word1)   
+          (recur (gen-next-layer candidates nodes)))  )) ))
+
--- doublets/test/doublets/solver_test.clj
+++ doublets/test/doublets/solver_test.clj
@@ -1,21 +1,79 @@
 (ns doublets.solver-test
-  (:require [clojure.test :refer :all]
-            [doublets.solver :refer :all]))
+    (:use midje.sweet)
+    (:require [clojure.test :refer :all]
+              [doublets.solver :refer :all]))
 
-(deftest solver-test
-  (testing "with word links found"
-    (is (= ["head" "heal" "teal" "tell" "tall" "tail"]
-           (doublets "head" "tail")))
+(facts "about is-transition? test"
+       (fact "a transition exists"
+             (is-transition? "head" "heal") => truthy
+             (is-transition? "book" "look") => truthy 
+             (is-transition? "...." "....") => falsey 
+             (is-transition? "aa.." "....") => falsey 
+             (is-transition? ".aa." "....") => falsey 
+             (is-transition? "..aa" "....") => falsey 
+             (is-transition? "a.a." "....") => falsey 
+             (is-transition? "...." "....") => falsey 
+             (is-transition? "a"    "b")    => truthy
+             (is-transition? "of"   "on")   => truthy
+             (is-transition? "a..." "....") => truthy 
+             (is-transition? ".a.." "....") => truthy 
+             (is-transition? "..a." "....") => truthy 
+             (is-transition? "...a" "....") => truthy )
+       (fact "a transition does not exist"
+             (is-transition? "head" "teal") => falsey
+             (is-transition? "bank" "cbok") => falsey )
+       (fact "a transition does not exist when the words are identical"
+             (is-transition? "head" "head") => falsey )
+       (fact "a transition does not exist when the first subword is a transition"
+             (is-transition? "bead" "heady") => falsey )
+       (fact "a transition does not exist when the second subword is a transition"
+             (is-transition? "heady" "bead") => falsey )
+       )
 
-    (is (= ["door" "boor" "book" "look" "lock"]
-           (doublets "door" "lock")))
+(facts "about node expansion"
+       (fact "expand a single relation"
+             (expand-node ["head"]) => [ ["heal" "head"] ])
+       (fact "expand a dual relation"
+             (expand-node ["heal"]) => (just [ ["teal" "heal"] ["head" "heal"] ] :in-any-order)) 
+       (fact "expand a zero relation"
+             (expand-node ["heart"]) => []) 
+       (fact "expand a multi multi relation and ensure no loops"
+             (expand-node ["heal" "head"]) => [ ["teal" "heal" "head"] ] )
+       (fact "check a terminating chain"
+             (expand-node ["head" "heal" "teal" "tell" "tall" "tail"]) => []) 
+       )
 
-    (is (= ["bank" "bonk" "book" "look" "loon" "loan"]
-           (doublets "bank" "loan")))
+(facts "about getting the next layer"
+       (fact "generate a single relation"
+             (gen-next-layer '( ["head"] ) ) => [ ["heal" "head"] ])
+       (fact "generate a multi relation"
+             (gen-next-layer '(["head"] ["heal"]) ) => (just [ ["heal" "head"] ["teal" "heal"] ["head" "heal"] ] :in-any-order))
+       (fact "generate a multi relation"
+             (gen-next-layer '(["heal" "head"] ["heal"]) ) => (just [ ["teal" "heal" "head"] ["teal" "heal"] ["head" "heal"] ] :in-any-order))
+       )
 
-    (is (= ["wheat" "cheat" "cheap" "cheep" "creep" "creed" "breed" "bread"]
-           (doublets "wheat" "bread"))))
+(facts "about finding a solution"
+       (fact "these chains have a solution"
+             (solution? '( ["heal" "head"] ["tail" "mail"] )  "heal") => ["heal" "head"]
+             (solution? '( ["tail" "mail"] ["heal" "head"] )  "heal") => ["heal" "head"]
+             )
+       (fact "these chains have no solution"
+             (solution? '( ["heal" "head"] ["tail" "mail"] )  "fail") => falsey
+             (solution? '( ["tail" "mail"] ["heal" "head"] )  "bob")  => falsey
+             )
+       (fact "these chains have two solutions, but the first is interesting"
+             (solution? '( ["heal" "head"] ["tail" "mail"] ["heal" "meal"] )  "heal") => ["heal" "head"]
+             (solution? '( ["tail" "mail"] ["heal" "head"] ["heal" "head"] )  "heal") => ["heal" "head"]
+             )
+       )
 
-  (testing "with no word links found"
-    (is (= []
-           (doublets "ye" "freezer")))))
+(facts "about solution"
+       (fact "with word links found"
+             (doublets "head" "tail")   => ["head" "heal" "teal" "tell" "tall" "tail"] 
+             (doublets "door" "lock")   => ["door" "boor" "book" "look" "lock"]
+             (doublets "bank" "loan")   => ["bank" "bonk" "book" "look" "loon" "loan"]
+             (doublets "wheat" "bread") => ["wheat" "cheat" "cheap" "cheep" "creep" "creed" "breed" "bread"] )
+
+       (fact "with no word links found"
+             (doublets "ye" "freezer") => falsey )
+       )
