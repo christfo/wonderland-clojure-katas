efb7a48f28b2ffba2bed0074bfd81fb67612aa2e
Chris Ford <chrisf@primate.com>
2016-09-14 15:26:50 +0100

    solution for fox, goose, corn. Had real trouble applying HOF from the pattern matched 'move/actions' Which I finally solved by switching from a quoted list of functions to and array of functions. I think this was due to: https://stackoverflow.com/questions/9760480/in-clojure-difference-between-function-quoted-function-and-sharp-quote-functio . Also learnt the need to wrap 'doall' around lazy seq's when trying to do side effects such as printing!

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,37 +1,60 @@
 (ns fox-goose-bag-of-corn.puzzle
   (:require [clojure.tools.logging :as log]))
 
-(def start-pos [[#{:fox :goose :corn :you} #{:boat} #{} ]] )
+(def start-pos [ #{:fox :goose :corn :you} #{:boat} #{} ] )
+(def end-pos   [ #{} #{:boat} #{:you :fox :corn :goose} ] )
+
+(defn solved? [path] (if (= end-pos (last path)) path))
 
 ; generate sets of items in each location
 (defn lhbank [[loc _ _]] loc )
 (defn boat   [[_ loc _]] loc )
 (defn rhbank [[_ _ loc]] loc )
-(defn cargo [things] (disj things :you :boat))
+(defn cargo [things] (conj  (disj things :you :boat) nil))
+
+; state transitions
+(defn  from  [current item] (disj  current item :you ))
+(defn  to    [current item] (disj (conj  current item :you) nil) )
+(defn  nop   [current item]  current) 
 
 
 (defn moves [gstate] 
-  (let [from             (fn [current items] (log/spy  (map  #(disj  current % :you) items)))
-        to               (fn [current items] (log/spy  (map  #(conj  current % :you) items)))
-        nop              (fn [current items] (log/spy  (map  (fn [_] current)        items))) 
-        current_loc      (map :you gstate)
-        [itemloc actions]  (case current_loc
-                           [:you nil  nil ] [lhbank  '([from to nop])]
-                           [nil  :you nil ] [boat    '([to from nop] [nop from to])]
-                           [nil  nil  :you] [rhbank  '([nop to from])] )
-        items            (cargo (itemloc gstate))
-        getnew           (fn [operation current]  (log/spy :info (operation current items)))]
-    ( map #(map getnew %1 gstate) actions )
-    ))
+  " generate the next layer of available states from the current one"
+  (let [
+        [itemloc actions]  (case (map :you gstate)
+                             [:you nil  nil ] [lhbank  [ [from to nop]               ] ]
+                             [nil  :you nil ] [boat    [ [to from nop] [nop from to] ] ]
+                             [nil  nil  :you] [rhbank  [ [nop to from]               ] ] )]
+    (for [ action  actions, item  (cargo (itemloc gstate)) ]
+      (map #(%1 %2 item) action  gstate ))))
 
 
-(defn turn [gstate]
-  ; find 'you'
-  ( let [hops (cond 
-    (some #{:you} (lhbank gstate)) [lhbank boat rhbank]
-    (some #(:you) (rhbank gstate)) [rhbank boat lhbank])]
-    hops)
-  )
+(defn legal [state] 
+  "take a state and test its legality / if it is terminal"
+  (let [ boat-capacity  #(> (count (cargo (boat %))) 2)   ; cargo has a nil which is counted 
+         goose-death    #(some (fn [loc] (and (loc :goose) (loc :fox)     (not ( loc :you)))) % ) 
+         corn-death     #(some (fn [loc] (and (loc :corn)    (loc :goose) (not ( loc :you)))) % ) 
+         many-any       #(not= 5 (reduce + (map count %)))
+         missing        #(not= #{:fox :goose :corn :you :boat} (reduce (fn [c i] (into c i)) #{} %))
+         boat-moved     #(not= [nil :boat nil] (map :boat %)) ]
+      (not ( (some-fn boat-capacity goose-death corn-death many-any missing boat-moved) state)) ))
+
+
+
+(defn ppath [ paths ]
+  "Game path pretty printer"
+  (println "state ------------------------" )
+  (doall (for [path paths] (do
+        (println "    path ------------------------" )
+        (doall (for [step path] (println "         - " step)))))))
 
 (defn river-crossing-plan []
-  start-pos)
+  "breadth first exhaustive search of each successive game state"
+  (loop [ paths  [[start-pos]] ]
+    (let [newpaths (reduce into [] 
+                           (for [path paths] 
+                             (let [newstates (moves (last path))
+                                   legalstates (into [] (filter legal newstates)) ]
+                               (for [state legalstates :when (not-any? #{state} path)] 
+                                 (conj path state))  ))) ]
+      (or (some solved? newpaths) (recur newpaths))))) 
--- fox-goose-bag-of-corn/test/fox_goose_bag_of_corn/puzzle_test.clj
+++ fox-goose-bag-of-corn/test/fox_goose_bag_of_corn/puzzle_test.clj
@@ -16,7 +16,10 @@
       step2)))
 
 (deftest test-river-crossing-plan
-  (let [crossing-plan (map (partial map set) (river-crossing-plan))]
+  ; (let [crossing-plan (map (partial map set) (river-crossing-plan))]
+  (let [crossing-plan ( river-crossing-plan )]
+  (println "solution ------------------------" )
+    (ppath crossing-plan)
     (testing "you begin with the fox, goose and corn on one side of the river"
       (is (= [#{:you :fox :goose :corn} #{:boat} #{}]
              (first crossing-plan))))
@@ -49,18 +52,51 @@
 (facts "about things in places"
        (fact "lh"
              (lhbank test_state) => #{:a :b :c :you}
-             (cargo (lhbank test_state) ) => #{:a :b :c})
+             (cargo (lhbank test_state) ) => #{:a :b :c nil})
        (fact "rh"
              (rhbank test_state) => #{:f :g}
-             (cargo (rhbank test_state )) => #{:f :g}
-             (cargo (rhbank test_state )) => #{:f :g})
+             (cargo (rhbank test_state )) => #{:f :g nil}
+             (cargo (rhbank test_state )) => #{:f :g nil})
        (fact "boat"
              (boat test_state) => #{:d :e :boat}
-             (cargo (boat test_state )) => #{:d :e}) )
+             (cargo (boat test_state )) => #{:d :e nil}) )
 
 (facts "about game steps"
        ; (def test_state [#{:a :b :c :you} #{:d :e :boat} #{:f :g}])
        (fact "lbank to boat"
-             (moves [#{:a :you} #{:boat} #{}] ) => '( [ #{} #{:boat :a :you} #{} ] ) 
-             ; (moves test_state  ) => '( [ #{} #{:boat :a :you} #{} ] ) 
+             (moves [#{ :andy :you } #{:boat  :bob } #{}] ) => (just [ #{:andy} #{:boat :you :bob} #{} ] 
+                                                                     [ #{} #{:boat :andy :you :bob} #{} ] ) 
+             ; (moves [#{ :fox :corn :you } #{:boat} #{}] ) => ( contains [ #{:fox} #{:boat :you :corn} #{} ] ) 
              ))
+
+(facts "about legal states"
+       (fact "legal postions true"
+             (legal [#{:corn } #{:you :boat :fox} #{:goose}] ) => truthy
+             (legal [#{:corn :fox } #{:you :goose :boat} #{}] ) => truthy)
+       (fact "boat too full"
+             (legal [#{} #{:you :boat :corn :fox} #{:goose}] ) => falsey)
+       (fact "too may you"
+             (legal [#{:corn :you} #{:you :boat :fox} #{:goose}] ) => falsey)
+       (fact "beached boat"
+             (legal [#{:corn :boat} #{:you :fox} #{:goose}] )   => falsey)
+       (fact "hungry goose"
+             (legal [#{:corn :goose } #{:you :boat :fox} #{}] ) => falsey)
+       (fact "hungry fox"
+             (legal [#{:corn :you } #{:boat } #{:goose :fox}] ) => falsey)
+       (fact "missing corn"
+             (legal [#{:jim} #{:boat } #{:you :goose :fox}] ) => falsey)
+       (fact "feast"
+             (legal [#{:corn :fox :goose} #{:you :boat} #{}] ) => falsey)
+       )
+
+(facts "about solution?"
+       (fact "is unsolved"
+             (solved? [start-pos])         => falsey
+             (solved? [:a start-pos])      => falsey
+             (solved? [end-pos start-pos]) => falsey )
+       (fact "is solved"
+             (solved? [ end-pos ])           => truthy
+             (solved? [ start-pos end-pos ]) => truthy )
+       )
+
+
