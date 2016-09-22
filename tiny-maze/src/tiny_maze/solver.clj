bb5bf36f161a57afb36ebbd73b6c2fe15e304473
Chris Ford <chrisf@primate.com>
2016-09-22 11:29:06 +0100

    working maze solver. Its breadth first recursion that fans out based on path choices. Happy with manipulating the path rather than the whole maze as I initially tried. Unhappy with it being breadth and not depth first. I think this is best solved by always searching right and down, and backing out of poor choices (assumption based on an old screen saver BTW). I aslo realised along the way that I should probably have used clojure/zip. I think I will try another solution with this instead.

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -1,3 +1,40 @@
 (ns tiny-maze.solver)
 
-(defn solve-maze [maze])
+(def tmaze [[:S 0 1]
+            [1  0 1]
+            [1  0 :E]] )
+
+(defn right [[w h] loc] (let [newloc (inc loc)] (if-not (zero? (mod newloc w)) newloc)))
+(defn left  [[w h] loc] (let [newloc (dec loc)] (if-not (zero? (mod loc w   )) newloc)))
+(defn up    [[w h] loc] (let [newloc (+ loc w)] (if-not (>= newloc (* w h)) newloc)))
+(defn down  [[w h] loc] (let [newloc (- loc w)] (if-not (neg? newloc) newloc)))
+(defn start [maze] (first  (keep-indexed #(when (= :S %2) %1) maze)))
+(defn end   [maze] (first  (keep-indexed #(when (= :E %2) %1) maze)))
+(defn valid? [maze loc] (when (and loc (zero? (nth maze loc))) loc))
+(defn take-step [maze loc] (concat (take loc maze) '(:x) (rest (nthrest maze loc ))))
+(defn lockops [ ops [w h :as dims] ] (map #( partial % dims ) ops ) )
+(defn solved? [end [loc & _ :as path]] (when  (= loc end) path))
+
+(defn turn-choices [ maze ops [loc & previous :as path] ]
+  (let [newlocs (keep #(valid? maze (% loc)) ops)]
+    (remove (set path) newlocs)) ) ; don't turn back on yourself
+
+(defn expand-path [ maze ops [loc & previous :as path] ]
+  (let [nextlocs (turn-choices maze ops path)] 
+    (for [nextloc nextlocs]  (cons nextloc path))))
+
+(defn solve-path [[w h :as dims] maze]
+  (let [ ops                  (lockops [right down left up] dims)
+        initial-path          [ (start maze) ]
+        solved?               (partial solved? (end maze))
+        maze                  (replace {:S 0 :E 0} maze)  ]
+    (loop [ paths [ initial-path ] ] 
+      (let [ newpaths  (mapcat #(expand-path maze ops %) paths)
+             solution  (some solved? newpaths) ]
+        (or solution (recur newpaths))))))
+
+(defn solve-maze [maze]
+  (let [solution     (solve-path [(count (first maze)) (count maze)] (flatten maze))
+        solved-maze  (reduce take-step (flatten maze) solution)]
+    (partition (count (first maze)) solved-maze)))
+
--- tiny-maze/test/tiny_maze/solver_test.clj
+++ tiny-maze/test/tiny_maze/solver_test.clj
@@ -1,6 +1,34 @@
 (ns tiny-maze.solver-test
+  (:use midje.sweet)
   (:require [clojure.test :refer :all]
             [tiny-maze.solver :refer :all]))
+
+(facts "about turn movement" 
+       (fact "moving right"
+             (right [3 3] 0) => 1
+             (right [3 3] 1) => 2
+             (right [3 3] 2) => nil
+             (right [3 3] 8) => nil)
+       (fact "moving left"
+             (left [3 3] 0) => nil
+             (left [3 3] 1) => 0
+             (left [3 3] 2) => 1
+             (left [3 3] 8) => 7)
+       (fact "moving up"
+             (up [3 3] 0) => 3
+             (up [3 3] 1) => 4
+             (up [3 3] 2) => 5
+             (up [3 3] 8) => nil)
+       (fact "moving up"
+             (down [3 3] 0) => nil
+             (down [3 3] 1) => nil
+             (down [3 3] 7) => 4
+             (down [3 3] 8) => 5)
+       )
+ (facts "about the maze"
+        (start [0 0 :S 0 nil 0 :x :E]) => 2
+        (end   [0 0 :S 0 nil 0 :x :E]) => 7
+        )
 
 (deftest test-solve-maze
   (testing "can find way to exit with 3x3 maze"
