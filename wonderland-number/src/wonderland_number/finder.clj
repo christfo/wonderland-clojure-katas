e1090e1a2a08795b84a2558af9d7d8c0727fa17f
Chris Ford <chrisf@primate.com>
2016-09-24 11:11:00 +0100

    wonderland numbers solved. Brute force solution, but saved by lazy map? Is worth revisiting with a threadpool.

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,17 @@
-(ns wonderland-number.finder)
+(ns wonderland-number.finder
+  (:require [ clojure.math.numeric-tower :as math]))
 
-(defn wonderland-number []
-  ;; calculate me
-  42)
+(defn match-digits? [n1 n2]  ( reduce = (map (comp set str) [n1 n2] )))
+(defn wonderland? [n] (when  (every? #(match-digits? n (* n %)) [2 3 4 5 6]) n) ) 
+
+(defn wonderland-list [digits]
+  (let [lowest  (math/expt 10 (dec digits))
+        highest (dec (math/expt 10 digits))] 
+    (->> (range lowest highest)
+         (map wonderland?)
+         (keep identity))))
+
+(defn wonderland-number 
+  ( []         (wonderland-number 6))             ; question is about 6 digit wonderlands
+  ( [digits]   (first (wonderland-list digits)))) ; apparently appending '0' to a wonderland is also a wonderland
+   
--- wonderland-number/test/wonderland_number/finder_test.clj
+++ wonderland-number/test/wonderland_number/finder_test.clj
@@ -1,4 +1,5 @@
 (ns wonderland-number.finder-test
+  (:use midje.sweet)
   (:require [clojure.test :refer :all]
             [wonderland-number.finder :refer :all]))
 
@@ -16,3 +17,20 @@
       (is (hasAllTheSameDigits? wondernum (* 4 wondernum)))
       (is (hasAllTheSameDigits? wondernum (* 5 wondernum)))
       (is (hasAllTheSameDigits? wondernum (* 6 wondernum))))))
+
+(facts "about match-digits"
+       (fact "different numbers with uncommon digits fail to match"
+             (match-digits? 1234 5678) => falsey )
+       (fact "equal numbers have the same digits"
+             (match-digits? 1234 1234) => truthy)       
+       (fact "rearranged numbers have the same digits"
+             (match-digits? 1234 4321) => truthy)       
+       (fact "numbers of unequal length with common digits match"
+             (match-digits? 1234 12344321) => truthy)       
+       )
+
+(facts "about wonderland-number"
+       (fact "142857 is a wonderland number"     (wonderland? 142857) => truthy)
+       (fact "the number is returned when found" (wonderland? 142857) => 142857)
+       (fact "123456 is not wonderland"          (wonderland? 123456) => falsey)
+       )
